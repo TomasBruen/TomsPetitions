7227ae20aac4e66c2973e2dab3c883edbf367196
TomasBruen <tomasbruen@gmail.com>
2026-04-13 22:02:28 +0100

    Finalized contextual html changes

--- src/main/java/com/tomspetitions/demo/HTMLController.java
+++ src/main/java/com/tomspetitions/demo/HTMLController.java
@@ -33,35 +33,41 @@ public class HTMLController {
 
     @RequestMapping("/viewAll")
     public String getAllPetitions(HttpServletRequest request){
-        return htmlStorage.viewPetitionsPage(inMemoryDB.findAll());
+        String contextPath = request.getContextPath();
+        return htmlStorage.viewPetitionsPage(inMemoryDB.findAll(),contextPath);
     }
 
     @RequestMapping("/sign/{id}")
     public String signPetition(HttpServletRequest request,@PathVariable int id){
-        return htmlStorage.signPetitionPage(inMemoryDB.findById(id).get());
+        String contextPath = request.getContextPath();
+        return htmlStorage.signPetitionPage(inMemoryDB.findById(id).get(),contextPath);
     }
 
     @RequestMapping("/new")
     public String createNewPetition(HttpServletRequest request){
-        return htmlStorage.createPetitionPage();
+        String contextPath = request.getContextPath();
+        return htmlStorage.createPetitionPage(contextPath);
     }
 
     @RequestMapping("/searchPetition")
     public String searchPage(HttpServletRequest request){
-        return htmlStorage.searchPetitionPage();
+        String contextPath = request.getContextPath();
+        return htmlStorage.searchPetitionPage(contextPath);
     }
 
     @GetMapping("/searchAction")
     public String searchAction(HttpServletRequest request,@RequestParam String title){
+        String contextPath = request.getContextPath();
         if(title.isEmpty() || title.length()<3){
-            return htmlStorage.notificationPage("Error: Invalid search term, has to have length gretter than 3.");
+            return htmlStorage.notificationPage("Error: Invalid search term, has to have length gretter than 3.",contextPath);
         }
         //System.out.println("withing search action.\n"+inMemoryDB.findByString(title).get(0).getTitle());
-        return htmlStorage.viewPetitionsPage(inMemoryDB.findByString(title));
+        return htmlStorage.viewPetitionsPage(inMemoryDB.findByString(title),contextPath);
     }
 
     @PostMapping("/signaction/{id}")
     public String signPetitionAction(HttpServletRequest request,@PathVariable int id, @RequestParam String signature, @RequestParam String email){
+        String contextPath = request.getContextPath();
         Petition petitionToUpdate = inMemoryDB.findById(id).get();
         ArrayList<String> updatedSignatures = petitionToUpdate.getSignatures();
         updatedSignatures.add(signature);
@@ -71,19 +77,21 @@ public class HTMLController {
         //System.out.println(updatedEmails);
         inMemoryDB.updatePetition(id,Optional.ofNullable(null),Optional.ofNullable(null),Optional.ofNullable(updatedSignatures),Optional.ofNullable(updatedEmails));
         
-        return htmlStorage.viewPetitionsPage(inMemoryDB.findAll());
+        return htmlStorage.viewPetitionsPage(inMemoryDB.findAll(),contextPath);
     }
 
     @GetMapping("/delete/{id}")
     public String deletePetitionAction(HttpServletRequest request,@PathVariable int id){
+        String contextPath = request.getContextPath();
         inMemoryDB.deletePetition(id);
-        return htmlStorage.viewPetitionsPage(inMemoryDB.findAll());
+        return htmlStorage.viewPetitionsPage(inMemoryDB.findAll(),contextPath);
     }
 
     @PostMapping(value="/newresult")
     public String createNewPetitionAction(HttpServletRequest request,@RequestParam String title, @RequestParam String description){
+        String contextPath = request.getContextPath();
         inMemoryDB.addNewPetition(title, description);
-        return htmlStorage.viewPetitionsPage(inMemoryDB.findAll());
+        return htmlStorage.viewPetitionsPage(inMemoryDB.findAll(),contextPath);
     }
 
     // Added this function to allow the test cases to check if the DB was changing correctly in response to 
--- src/main/java/com/tomspetitions/demo/HTMLStorage.java
+++ src/main/java/com/tomspetitions/demo/HTMLStorage.java
@@ -16,7 +16,7 @@ public class HTMLStorage {
             </head>
             <body class='bodyColor'>
         """;
-        myHTML += navBar();            
+        myHTML += navBar(contextPath);            
         myHTML +="""
             <h1 class="homeGreetText"> Welcome to Tom's petition project</h1>
             </body>
@@ -26,60 +26,63 @@ public class HTMLStorage {
     }
 
     // Returns the html page for inputting and submitting a new petition
-    public String createPetitionPage(){
+    public String createPetitionPage(String contextPath){
         String myHTML = """
             <html>
-            <head>
-                <link rel="stylesheet" href="/myStyle.css">
+            <head>""";
+        myHTML += "<link rel='stylesheet' href='"+contextPath+"/myStyle.css'>";
+        myHTML+="""
             </head>
             <body class='bodyColor'>
             """;
-        myHTML += navBar();            
-        myHTML +="""
-                <form action="/newresult" method="post" class="myColumnContainer">
+        myHTML += navBar(contextPath);            
+        myHTML +=String.format("""
+                <form action="%s/newresult" method="post" class="myColumnContainer">
                     <input name="title" value="Title" class="descriptionBox">
                     <input name="description" value="description" class="descriptionBox">
                     <button class="updateButton" type="submit"> Submit </button>
                 </form>
             </body>
             </html>
-            """;
+        """,contextPath);
         return myHTML;
     }
 
     // Returns the html page containing the form used to search for specific petitions
-    public String searchPetitionPage(){
+    public String searchPetitionPage(String contextPath){
         String myHTML = """
             <html>
-            <head>
-                <link rel="stylesheet" href="/myStyle.css">
+            <head>""";
+        myHTML += "<link rel='stylesheet' href='"+contextPath+"/myStyle.css'>";
+        myHTML+="""
             </head>
             <body class='bodyColor'>
             """;
-        myHTML += navBar();                    
-        myHTML +="""
-                <form action="/searchAction" method="get" class = "myColumnContainer">
+        myHTML += navBar(contextPath);                    
+        myHTML +=String.format("""
+                <form action="%s/searchAction" method="get" class = "myColumnContainer">
                     <input name="title" value="Search Term" class="descriptionBox">
                     <button class="updateButton" type="submit"> Submit </button>
                 </form>
             </body>
             </html>    
-        """;
+        """,contextPath);
 
         return myHTML;
     }
 
     // This returns the display page for petitions. It takes in a list of petitions and creates rows in a table for each petition passed.
-    public String viewPetitionsPage(ArrayList<Petition> petitions){
+    public String viewPetitionsPage(ArrayList<Petition> petitions, String contextPath){
         
         String myHTML = """
             <html>
-            <head>
-                <link rel="stylesheet" href="/myStyle.css">
+            <head>""";
+        myHTML += "<link rel='stylesheet' href='"+contextPath+"/myStyle.css'>";
+        myHTML+="""
             </head>
             <body class='bodyColor'>
         """;
-        myHTML += navBar();                    
+        myHTML += navBar(contextPath);                    
         myHTML +="""
                 <table class='tableStyle'>
                     <tr>
@@ -102,10 +105,10 @@ public class HTMLStorage {
                     <td class="descriptionCol"> %s </td>
                     <td class="listCol"> %s </td>
                     <td class="listCol"> %s </td>
-                    <td class="buttonCol"> <a href='/sign/%d'><button class="updateButton">Sign</button></a> </td>
-                    <td class="buttonCol"><a href='/delete/%d'><button class="deleteButton">Delete</button></a> </td>
+                    <td class="buttonCol"> <a href='%s/sign/%d'><button class="updateButton">Sign</button></a> </td>
+                    <td class="buttonCol"><a href='%s/delete/%d'><button class="deleteButton">Delete</button></a> </td>
                 </tr>        
-            """,petition.getTitle(),petition.getDescription(),signatureString,emailString,petition.getId(),petition.getId());
+            """,petition.getTitle(),petition.getDescription(),signatureString,emailString,contextPath,petition.getId(),contextPath,petition.getId());
         }
                             
         myHTML +="""
@@ -118,22 +121,23 @@ public class HTMLStorage {
     }
 
     // This function displays a single petition with form inputs to add a new signature and email.
-    public String signPetitionPage(Petition petition){
+    public String signPetitionPage(Petition petition, String contextPath){
         String myHTML = """
             <html>
-            <head>
-                <link rel="stylesheet" href="/myStyle.css">
+            <head>""";
+        myHTML += "<link rel='stylesheet' href='"+contextPath+"/myStyle.css'>";
+        myHTML+="""
             </head>
             <body class='bodyColor'>
         """;
-        myHTML += navBar();                    
+        myHTML += navBar(contextPath);                    
         myHTML += String.format(""" 
             <div class='myColumnContainer'>
                 <h1 style="margin:0;">Title</h1>
                 <h3 class="titleBox">%s</h3><br>
                 <h1 style="margin:0;">Description</h1>
                 <p class="descriptionBox">%s</p><br>
-                <form action="/signaction/%d" method="post" class="myColumnContainer">
+                <form action="%s/signaction/%d" method="post" class="myColumnContainer">
                     <input name="signature" value="Signature" class="signatureForm">
                     <input name="email" value="name@webpage.ie" type="email" class="emailForm">
                     <button class="updateButton" type="submit" class="updateButton"> Submit </button>
@@ -141,21 +145,22 @@ public class HTMLStorage {
             </div>
             </body>
             </html>
-        """,petition.getTitle(),petition.getDescription(),petition.getId());
+        """,petition.getTitle(),petition.getDescription(),contextPath,petition.getId());
         
         return myHTML;
     }
 
     // This page is simply used to display any notification passed to it as a string
-    public String notificationPage(String notification){
+    public String notificationPage(String notification, String contextPath){
         String myHTML = """
             <html>
-            <head>
-                <link rel="stylesheet" href="/myStyle.css">
+            <head>""";
+        myHTML += "<link rel='stylesheet' href='"+contextPath+"/myStyle.css'>";
+        myHTML+="""
             </head>
             <body class='bodyColor'>
         """;
-        myHTML += navBar();            
+        myHTML += navBar(contextPath);            
         myHTML += String.format("""
                 <h3>%s</h3>
             </body>
@@ -165,20 +170,20 @@ public class HTMLStorage {
     }
 
     // This function returns the html that defines the navigation bar used in all other pages
-    public String navBar(){
-        String myHTML = """
+    public String navBar(String contextPath){
+        String myHTML = String.format("""
             <div class='myRowContainer'>
-                <a href="/new">
+                <a href="%s/new">
                     <button class="navButton">Create Petition</button>
                 </a>
-                <a href="/viewAll">
+                <a href="%s/viewAll">
                     <button class="navButton">View All Petitions</button>
                 </a>
-                <a href="/searchPetition">
+                <a href="%s/searchPetition">
                     <button class="navButton">Search Petitions</button>
                 </a>
             </div>    
-            """;
+        """,contextPath,contextPath,contextPath);
         return myHTML;
     }
 
